d40ab56e6ff7f97c9ce85c6bdc0497b29700ef00
LauriVallee <150392341+LauriVallee@users.noreply.github.com>
2024-11-24 14:58:58 -0500

    puzzle fini

--- Gamerella/Assets/Scripts/Tile.cs
+++ Gamerella/Assets/Scripts/Tile.cs
@@ -5,4 +5,12 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     public int index;
+    public Vector2Int coords;
+    public Vector2Int winCoords;
+    public GameManager gameManager;
+
+    public void OnMouseDown()
+    {
+        gameManager.trySwap(this, true);
+    }
 }
